f2adb61d29ae5d559ce0063cef73ca7c95714a37
willharvv <112087181+willharvv@users.noreply.github.com>
2022-12-21 11:19:59 +0000

    steel cost improved and with rounding function

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
+alert ('enter all values in meters(m)')
 let length = prompt ('Length...')
 let height = prompt ('Height...')
 let width = prompt ('Width...')
     
-    const size = 2.1
-    const sheetcost = 20
+    const size = 2.1 //sheet size 
+    const sheetcost = 20 
     
     const baysi = 8
-    const priceOfSteel = 10
+    const priceOfSteel = 2.59 //per kg 
 
 
 
         let sheets = (length * width / size )  //take in consideration pitch and overlap
         roofcost = (sheets * sheetcost)
 
-        let bays = (length / baysi)   //height and length into metric tones
-        m2 = (height * width * bays) 
-        let steelcost = (m2 * priceOfSteel)
+        let bays = (length / baysi)  /// finds number of bays 
+         hkg = (height * 23 ) ///height to kg 
+         wkg = (width * 23)  ///width to kg 
+         kg = (hkg + wkg) //gives kg of height and width 
+        totalkg = (kg * bays)  // total weight 
+        let steelcost = (totalkg * priceOfSteel)
 
         scramount = sheets * 4
         const scrprice = 0.5
@@ -23,6 +27,13 @@ let width = prompt ('Width...')
 
     
 
-finalcost =( roofcost + steelcost + screwcost)
+cost =( roofcost + steelcost + scrcost)
+
+function roundToTwoDecimals(num) {
+    let rounded = Math.round(num * 100) / 100;
+    return rounded.toFixed(2);
+  }
+  finalcost = roundToTwoDecimals(cost)
+
 console.log('£' + finalcost)
 
